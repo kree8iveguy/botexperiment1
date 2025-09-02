4168e2c9eb7198af54ffe04ef385f9c918bb7e04
kree8iveguy <ctimmons1@gmail.com>
2025-09-02 19:58:57 -0400

    Update core_trend.py

--- src/core_trend.py
+++ src/core_trend.py
@@ -1,5 +1,6 @@
-def trend_signal(df):
-    """Simple moving average crossover strategy."""
+import pandas as pd
+
+def generate_signal(df: pd.DataFrame) -> str:
     if df["ma_fast"].iloc[-1] > df["ma_slow"].iloc[-1]:
         return "buy"
     elif df["ma_fast"].iloc[-1] < df["ma_slow"].iloc[-1]:
